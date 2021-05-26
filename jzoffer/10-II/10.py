cae9d0fa740f0424495b977151f43846419c8b2d
mraiser <mraiser@foxmail.com>
2021-05-26 15:39:18 +0800

    jzoffer 10-II AC

---
+++ jzoffer/10-II/10.py
@@ -0,0 +1,19 @@
+import functools
+
+@functools.cache
+def f(n: int) -> int:
+    if n <= 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return (f(n-1) + f(n-2)) % 1000000007
+
+class Solution:
+    def fib(self, n: int) -> int:
+        return f(n)
+
+
+print(f(2))
+print(f(7))
+print(f(0))
