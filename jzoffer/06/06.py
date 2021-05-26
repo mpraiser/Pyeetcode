180d5ecf2e3dae623b1b566acbfe5a5b0f116f1d
mraiser <mraiser@foxmail.com>
2021-05-26 15:18:57 +0800

    jzoffer 06 AC

---
+++ jzoffer/06/06.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+        return stack[::-1]
