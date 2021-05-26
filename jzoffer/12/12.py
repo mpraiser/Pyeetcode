b85d5ea2e84b69edf5e1d70305de8f18e69fc779
mraiser <mraiser@foxmail.com>
2021-05-26 17:49:23 +0800

    jzoffer 12 AC

---
+++ jzoffer/12/12.py
@@ -0,0 +1,48 @@
+from typing import List
+
+def is_connected(p1, p2):
+    # 2-norm, d4 connect
+    if p1 and p2:
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) == 1
+    else:
+        return True
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if board:
+            m = len(board)
+        else:
+            return False
+        if board[0]:
+            n = len(board[0])
+        else:
+            return False
+
+        p_used = {}
+
+        def helper(p, i):
+            """
+            Args:
+                p: point of last char
+                i: index of char to find in word
+            """
+            # end of search
+            if i >= len(word):
+                return True
+
+            for a in range(m):
+                for b in range(n):
+                    if ((a, b) not in p_used) or (not p_used[(a, b)]):
+                        if board[a][b] == word[i] and is_connected(p, (a, b)):
+                            p_used[(a, b)] = True
+                            if helper((a,b), i+1):
+                                return True
+                            p_used[(a, b)] = False
+            return False
+        
+        return helper(None, 0)
+
+
+x = Solution()
+ans = x.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED")
+print(ans)
