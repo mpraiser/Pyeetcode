1936f113419a13d6393885db30c51184c40751d6
mraiser <mraiser@foxmail.com>
2021-06-03 09:36:09 +0800

    space optimization: use generator instead of list

--- 525/525.py
+++ 525/525.py
@@ -4,9 +4,9 @@ from itertools import accumulate
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        converted_nums = [-1 if i == 0 else 1 for i in nums]
+        converted_nums = (-1 if i == 0 else 1 for i in nums)
 
-        sums = list(accumulate(converted_nums))
+        sums = accumulate(converted_nums)
         sums2index = {}
 
         max_distance = 0
@@ -24,7 +24,7 @@ class Solution:
 
 
 x = Solution()
-ans = x.findMaxLength([0,1,0])
-# ans = x.findMaxLength([0,0, 0, 1, 1, 1 , 1, 0, 0, 0, 0])
+# ans = x.findMaxLength([0,1,0])
+ans = x.findMaxLength([0,0, 0, 1, 1, 1 , 1, 0, 0, 0, 0])
 # ans = x.findMaxLength([0, 1])
 print(ans)
