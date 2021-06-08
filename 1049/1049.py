b1950599ce8007de7a97500a94b741a5074c8059
mraiser <mraiser@foxmail.com>
2021-06-08 18:15:02 +0800

    update 1049 notes

--- 1049/1049.py
+++ 1049/1049.py
@@ -4,44 +4,29 @@ from functools import cache
 
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
+        total = sum(stones)
         @cache
-        def helper(s: Tuple) -> int:
+        def f(i, t):
             """
-            best weight
+            Args:
+                i: first i-th stones
+                t: constraint, max sum
+            Returns:
+                max sum in first i-th stones, which is less than t
             """
-            if len(s) <= 2:
-                if len(s) == 0:
-                    return 0
-                if len(s) == 1:
-                    return s[0]
-                if len(s) == 2:
-                    return abs(s[0] - s[1])    
-            
-            # len(s) >= 3, keep search
-            solutions = []
-            # pick two nums
-            for i in range(len(s) - 1):
-                for j in range(i + 1, len(s)):
-                    if s[i] > s[j]:
-                        next_s = (s[m] if m != i else s[i] - s[j] for m in range(len(s)) if m != j)
-                    elif s[i] < s[j]:
-                        next_s = (s[m] if m != j else s[j] - s[i] for m in range(len(s)) if m != i)
-                    else:
-                        next_s = (s[m] for m in range(len(s)) if m !=i and m != j)
-                    
-                    weight = helper(tuple(next_s))
-                    if weight == 0:
-                        return 0
-                    solutions.append(weight)
-            return min(solutions)
-        
-        return helper(tuple(stones))
+            if i == 0:
+                return 0
+            ans = f(i-1, t)
+            if t >= stones[i-1]:
+                ans = max(ans, f(i-1, t-stones[i-1]) + stones[i-1])
+            return ans
+        return total - 2 * f(len(stones), total/2)
 
 
 
 x = Solution()
 # ans = x.lastStoneWeightII([2,7,4,1,8,1])
-ans = x.lastStoneWeightII([31,26,33,21,40])
-# ans = x.lastStoneWeightII([1,2])
+# ans = x.lastStoneWeightII([31,26,33,21,40])
+ans = x.lastStoneWeightII([1,1,4,2,2])
 # ans = x.lastStoneWeightII([1,1,2,3,5,8,13,21,34,55,89,14,23,37,61,98])
 print(ans)
