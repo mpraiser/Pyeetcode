9c412b4a3b629b8c3a43490433f44c90afb023ad
mraiser <mraiser@foxmail.com>
2021-06-10 15:35:42 +0800

    use dp

--- 518/518.py
+++ 518/518.py
@@ -2,19 +2,24 @@ from typing import List
 from functools import cache
 
 
-
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def f(i, t):
-            """
-            return numbers of solutions selecting in first i-th coins whose sum is t
-            t <= amount
-            """
-            if i == 1:
-                return 1 if t % coins[i-1] == 0 else 0
-            return sum((f(i-1, t - n * coins[i-1]) for n in range(int(t / coins[i-1] + 1))))
-        return f(len(coins), amount)
+        dp = [0] * (amount + 1)  # number of solutions, index as amount
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] += dp[i-coin]
+        return dp[amount]
+
+
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         @cache
+#         def f(t):
+#             if t == 0:
+#                 return 1
+#             return sum((f(t-coin) for coin in coins if t-coin >= 0))
+#         return f(amount)
 
 x = Solution()
 ans = x.change(3,  [2, 1])
