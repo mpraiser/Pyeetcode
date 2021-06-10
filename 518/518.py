ccc1414ef4ea5596a15ad4668579fc415d5feba2
mraiser <mraiser@foxmail.com>
2021-06-10 15:09:02 +0800

    use generator to simplify code (not space or time optimization)

--- 518/518.py
+++ 518/518.py
@@ -2,6 +2,7 @@ from typing import List
 from functools import cache
 
 
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         @cache
@@ -12,15 +13,9 @@ class Solution:
             """
             if i == 1:
                 return 1 if t % coins[i-1] == 0 else 0
-            # how many new coins are selected
-            ans = 0
-            n = 0
-            while (s_n := n * coins[i-1]) <= t:
-                ans += f(i-1, t - s_n)
-                n += 1
-            return ans
+            return sum((f(i-1, t - n * coins[i-1]) for n in range(int(t / coins[i-1] + 1))))
         return f(len(coins), amount)
 
 x = Solution()
-ans = x.change(3,  [2])
+ans = x.change(3,  [2, 1])
 print(ans)
