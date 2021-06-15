df3eb6f310f595595e0e5c7feb1b6115907e40fc
mraiser <mraiser@foxmail.com>
2021-06-15 16:53:59 +0800

    update 852 with binary search

--- 852/852.py
+++ 852/852.py
@@ -3,19 +3,25 @@ from typing import List
 
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        state = 'up'
-        last = arr[0]
-        peak = None
-        for i in range(1, len(arr)):
-            if state == 'up':
-                if arr[i] < last:
-                    state = 'down'
-                    peak = i-1
-                if state == 'down':
-                    if arr[i] > last:
-                        state = 'up'
-            last = arr[i]
-        return peak
+        left = 0
+        right = len(arr) - 1
+        ans = None
+
+        while left <= right:
+            mid = (left + right) // 2
+            # mid is peak or on downside
+            diff = arr[mid + 1] - arr[mid]
+            if diff < 0:
+                ans = mid
+                right = mid - 1
+            # mid is on upside
+            elif diff > 0:
+                left = mid + 1
+            # else:
+            #     raise Exception
+            
+        return ans
+
 
 x = Solution()
 ans = x.peakIndexInMountainArray( [24,69,100,99,79,78,67,36,26,19])
