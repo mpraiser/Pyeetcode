849590493a28e818dc11b9d3061643ea2a625f8b
mraiser <mraiser@foxmail.com>
2021-06-22 16:50:31 +0800

    jzoffer 42, use generator

--- jzoffer/42/42.py
+++ jzoffer/42/42.py
@@ -1,16 +1,16 @@
 from typing import List, Tuple
-from functools import cache
 
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        dp = [nums[0]]
-        for i in range(1, len(nums)):
-            if dp[i-1] <= 0:
-                dp.append(nums[i])
-            else:
-                dp.append(nums[i] + dp[i-1])
-        return max(dp)
+        def dp():
+            last = nums[0]
+            yield last
+            for i in range(1, len(nums)):          
+                last = nums[i] + max(last, 0)
+                yield last
+        return max(dp())
+
 
 ans = Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
 print(ans)
