383b2979175d8f0ca202437fc303f2b242f0addb
mraiser <mraiser@foxmail.com>
2021-06-28 14:04:50 +0800

    jzoffer 752 not finished; hot100 1 AC

--- 752/752.py
+++ 752/752.py
@@ -1,16 +1,5 @@
-from typing import List, Tuple
-from functools import cache
-
-
-def n_diff(a: str, b: str):
-    """number of differences in a and b"""
-    assert len(a) == len(b)
-    n = len(a)
-    counter = 0
-    for i in range(n):
-        if a[i] != b[i]:
-            counter += 1
-    return counter
+from typing import List
+from queue import Queue
 
 
 class Solution:
@@ -21,38 +10,44 @@ class Solution:
         """
         n = len(target)
         deadends = set(deadends)
-        basic_choices = set((str(i) for i in range(10)))
 
-        cache = {}
+        cache = dict()
         cache["0000"] = 0
         for dead in deadends:
             cache[dead] = -1
 
-        def f(current: str) -> int:
+        for index in range(n):
+            for i in range(1, 10):
+                cache[f"{i * 10 ** index:04d}"] = i
+
+        def f(current: str, visited: set) -> int:
             """Returns n-times to reach target"""
             if current in cache and cache[current] is not None:
                 return cache[current]
             
-            solutions = []
+            queue = Queue()
             for i in range(n):
                 # fix 1 bit, change other bits
-                if current[i] == "0":
-                    for j in (m for m in range(n) if m != i):
-                        choices = [str((int(current[j]) - 1) % 10), str((int(current[j]) + 1) % 10)]
-                        for choice in choices:
-                            latter = "".join(
-                                (item if index != j else choice for index, item in enumerate(current))
-                                )
-                            if latter not in cache:
-                                cache[latter] = None
-                                solution = f(latter) + 1
-                                cache[latter] = solution
-                                if solution != -1:
-                                    solutions.append(solution) 
-                            elif cache[latter] is not None:
-                                if cache[latter] != -1:
-                                    solutions.append(cache[latter] + 1) 
-            
+                choices = [str((int(current[i]) - 1) % 10), str((int(current[i]) + 1) % 10)]
+                for choice in choices:
+                    latter = "".join(
+                        (item if index != i else choice for index, item in enumerate(current))
+                        )
+                    if latter not in cache and latter not in visited:
+                        queue.put(latter)
+
+            while
+                        cache[latter] = None
+                        visited = visited.copy()
+                        visited.add(latter)
+                        solution = f(latter, visited) + 1
+                        cache[latter] = solution
+                        if solution != -1:
+                            solutions.append(solution)
+                    elif cache[latter] is not None:
+                        if cache[latter] != -1:
+                            solutions.append(cache[latter] + 1)
+            solutions = []
             if solutions:
                 result = min(solutions)
                 cache[current] = result
@@ -61,7 +56,8 @@ class Solution:
                 cache[current] = -1
                 return -1
 
-        return f(target)
+        return f(target, set())
+
 
 ans = Solution().openLock(["0201","0101","0102","1212","2002"], "0202")
 print(ans)
