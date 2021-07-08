24dcc99c2e632faca63c51eca50bfd9100a1843b
mraiser <mraiser@foxmail.com>
2021-07-08 17:40:04 +0800

    18 AC

--- main/18.py
+++ main/18.py
@@ -1,23 +1,38 @@
 from typing import List
-from itertools import combinations
+
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         if len(nums) < 4:
             return []
-        result = set()
-        nums.sort()
-        for a in range(len(nums)-3):
-            for b in range(a+1, len(nums)-2):
-                for c in range(b+1, len(nums)-1):
-                    for d in range(c+1, len(nums)):
-                        four = (nums[a], nums[b], nums[c], nums[d])
-                        if sum(four) == target:
-                            result.add(four)
-        return [list(x) for x in result]
 
-x = Solution()
-# ans = x.fourSum([1,0,-1,0,-2,2], 0)
-# ans = x.fourSum([2,2,2,2,2], 8)
-ans = x.fourSum([-5,5,4,-3,0,0,4,-2], 4)
+        nums.sort()
+        ret = []
+        for i in range(len(nums)-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)-2):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue
+                t = target - nums[i] - nums[j]
+                m = j + 1
+                n = len(nums) - 1
+                while m < n:
+                    s = nums[m] + nums[n]
+                    if s == t:
+                        ret.append([nums[i], nums[j], nums[m], nums[n]])
+                        while m < n and nums[m] == nums[m+1]:
+                            m += 1
+                        m += 1
+                        while m < n and nums[n] == nums[n-1]:
+                            n -= 1
+                        n -= 1
+                    elif s < t:
+                        m += 1
+                    elif s > t:
+                        n -= 1
+        return ret
+                
+       
+ans = Solution().fourSum([1,0,-1,0,-2,2], 0)
 print(ans)
