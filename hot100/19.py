72fb554f9f22e26d15dfd90a880d1a017ad08203
mraiser <mraiser@foxmail.com>
2021-07-09 15:04:07 +0800

    hot100.19 AC

---
+++ hot100/19.py
@@ -0,0 +1,28 @@
+from utils import ListNode
+from collections import deque
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dq = deque()
+        node = head
+        while node:
+            dq.append(node)
+            node = node.next
+        if n == len(dq):
+            if len(dq) == 1:
+                return None
+            else:
+                return dq[1]
+        elif n == 1:
+            dq[-2].next = None
+            return dq[0]
+        else:
+            dq[-n-1].next = dq[-n+1]
+            return dq[0]
+
+
+# ans = Solution().removeNthFromEnd(ListNode.parse([1,2,3,4,5]), 2)
+ans = Solution().removeNthFromEnd(ListNode.parse([1, 2]), 1)
+print(ans)
+print(ans.serialize())
