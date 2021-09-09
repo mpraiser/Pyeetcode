55301feb59e0acaa0447e511cb834091d913c124
mraiser <mraiser@foxmail.com>
2021-09-09 10:40:55 +0800

    1143 AC.

---
+++ main/1143.py
@@ -0,0 +1,29 @@
+from functools import cache
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        # @cache
+        # def lcs(i: int, j: int) -> int:
+        #     if i == 0 or j == 0:
+        #         return 0
+        #     if text1[i - 1] == text2[j - 1]:
+        #         return lcs(i - 1, j - 1) + 1
+        #     else:
+        #         return max(lcs(i - 1, j), lcs(i, j - 1))
+        #
+        # return lcs(len(text1), len(text2))
+
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[len(text1)][len(text2)]
+
+
+ans = Solution().longestCommonSubsequence("abcde", "ace")
+print(ans)
